99af6e17d77d641b58f94f3d215035596e64e30c
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-09-14 13:53:29 +0100

    added files

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import {addToCart, removeFromCart} from '../actions/CartActions';
 import {useDispatch, useSelector} from 'react-redux';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 function CartScreen (props) {
    const cart = useSelector(state => state.cart);
